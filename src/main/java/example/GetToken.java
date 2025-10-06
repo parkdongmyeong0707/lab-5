9896f6103eaa29300f51e980d50b5695a646e932
unknown <white040707@gmail.com>
2025-10-06 16:31:57 -0400

    did the task 1

--- src/main/java/example/GetToken.java
+++ src/main/java/example/GetToken.java
@@ -1,5 +1,7 @@
 package example;
 
+import java.io.IOException;
+
 /**
  * Class for getting your Grade API token.
  */
@@ -9,13 +11,12 @@ public class GetToken {
      * Run to get your token after you complete the code.
      * @param args unused arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         GetExample example = new GetExample();
 
-        String gradeAPISignUpURL = "https://grade-apis.panchen.ca/signUp";
+        String gradeAPISignUpURL = "https://grade-apis.panchen.ca/signUp=team06";
         // TODO Task 1: use the run method to get your Grade API token (or obtain your token through a web browser).
-        String response = "";
-        // response = example.run( ...)
+        String response = example.run("https://grade-apis.panchen.ca/signUp?username=team06");
         System.out.println(response);
     }
 }
